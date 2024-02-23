590fba3c97d4df32f763d29c93261a0984394412
Bryan <bryanjosueriverahernandez@gmail.com>
2024-02-22 22:51:05 -0600

    Switch2

--- TAREA3/Program.cs
+++ TAREA3/Program.cs
@@ -1,37 +1,42 @@
-﻿try { 
-Console.WriteLine("Ingrese el tipo de figura geométrica (triángulo, cuadrado o círculo):");
-string Figura = Console.ReadLine().ToLower();
-double area = 0;
+﻿try
+{ 
+    Console.WriteLine("Ingrese un número del 1 al 7:");
+    int numeroDia = Convert.ToInt32(Console.ReadLine());
 
-switch (Figura)
-{
-    case "triangulo":
-        Console.WriteLine("Ingrese la base:");
-        double basetriangulo = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Ingrese la altura:");
-        double alturatriangulo = Convert.ToDouble(Console.ReadLine());
-        area = 0.5 * basetriangulo * alturatriangulo;
-        break;
-    case "cuadrado":
-        Console.WriteLine("Ingrese el lado del cuadrado:");
-        double ladocuadrado = Convert.ToDouble(Console.ReadLine());
-        area = ladocuadrado * ladocuadrado;
-        break;
-    case "circulo":
-        Console.WriteLine("Ingrese el radio del círculo:");
-        double radiocirculo = Convert.ToDouble(Console.ReadLine());
-        area = Math.PI * radiocirculo * radiocirculo;
-        break;
-    default:
-        Console.WriteLine("Figura Geometrica no valida");
-        return;
-}
+    string dia = "";
+    switch (numeroDia)
+    {
+        case 1:
+            dia = "Lunes";
+            break;
+        case 2:
+            dia = "Martes";
+            break;
+        case 3:
+            dia = "Miércoles";
+            break;
+        case 4:
+            dia = "Jueves";
+            break;
+        case 5:
+            dia = "Viernes";
+            break;
+        case 6:
+            dia = "Sábado";
+            break;
+        case 7:
+            dia = "Domingo";
+            break;
+        default:
+            Console.WriteLine("Número de día no válido.");
+            return;
+    }
 
-Console.WriteLine($"El área de la figura geométrica {Figura} es: {area}");
+    Console.WriteLine($"El día de la semana correspondiente a {numeroDia} es: {dia}");
 }
 catch (FormatException)
 {
-    Console.WriteLine("Error: Por favor, ingrese valores numéricos válidos.");
+    Console.WriteLine("Ingrese solo numeros validos.");
 }
 catch (Exception ex)
 {
